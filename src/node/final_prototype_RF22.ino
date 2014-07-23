6a3b9daa1f659b386132802367f8545ac12e63e9
Omar Ibrahim Hussain <moihussain@gmail.com>
2014-07-23 22:05:01 +0530

    CleanUp: Removed code related to RF24

--- src/node/final_prototype_RF22.ino
+++ src/node/final_prototype_RF22.ino
@@ -16,34 +16,20 @@
 /************Public Includes************************/
 #include <EEPROM.h>
 #include <SPI.h>
-///#include "RF24.h"
 #include <RF22.h>
 
-///RF24 radio(7,8);
 RF22 rf22;
 //Define number of switches controlled by the node
-#define N 4
-//Defining pins on Arduino through which switches are controlled
 
-/*
-#define SWITCH1_PIN 8
-#define SWITCH2_PIN 9
-#define SWITCH3_PIN 10
-#define SWITCH4_PIN 11
-*/
+#define N 4
+
+//Defining pins on Arduino through which switches are controlled
 
 const unsigned short int switch_pins[N]={8,9,10,11};	//for 4 switch node
 
 /* Defining starting addresses in EEPROM where each of the switches' state and/or other properties are stored.
    Assuming 20 bytes would suffice for each switch data. Can extend if needed.
    Here addresses 20-39 : SWITCH1, 40-59 : SWITCH2, 60-79 : SWITCH3, 80-99 : SWITCH4 	*/
-
-/*
-#define SWITCH1_EEPROM_START_ADDR 20
-#define SWITCH2_EEPROM_START_ADDR 40
-#define SWITCH3_EEPROM_START_ADDR 60
-#define SWITCH4_EEPROM_START_ADDR 80
-*/
 
 const unsigned short int switch_addr[N]={20,30,40,50};	//for 4 switch node 
 const unsigned short int commom_info_addr=10;           //starting address to store common information such as NODE_ID,MASTER_ID,LAST_COMMAND_ID,LAST_KEEPALIVE_ID,MANUAL_OVERRIDE
@@ -73,7 +59,7 @@ unsigned short int num_properties,num_commands;
 
 bool data_to_be_processed=false;
 
-//Every switch has a pin,state and EEPROM start address associated with it
+//Every switch has a pin, state and EEPROM start address associated with it
 struct switches
 {
 unsigned short int pin;
@@ -127,13 +113,9 @@ void RF_Init()
            Serial.println("ROLE:  SLAVE NODE");
            Serial.println("");
 
-///        radio.begin();
-///        radio.setRetries(15,15);
-///        radio.openReadingPipe(1,pipes[0]);
-///        radio.startListening();
       if (!rf22.init())
         Serial.println("RF22 init failed");
-        // Defaults after init are 434.0MHz, 0.05MHz AFC pull-in, modulation FSK_Rb2_4Fd36        
+        // TODO: Check (probably comment from old code): Defaults after init are 434.0MHz, 0.05MHz AFC pull-in, modulation FSK_Rb2_4Fd36        
 }
 
 void Read_EEPROM()
@@ -141,6 +123,8 @@ void Read_EEPROM()
 //TEMPORARY ASSIGNMENT FOR NODE_ID AND MASTER_ID
         NODE_ID=0x46;
         MASTER_ID=0x41;
+		//TODO: Master ID and Node ID reading is fine. Check where these are assigned and stored in EEPROM.
+		//TODO: Also check if it can have clearly defined separate EEPROM addresses instead of common_info_addr
 //        NODE_ID=EEPROM.read(commom_info_addr);
 //        MASTER_ID=EEPROM.read(commom_info_addr+1);
 //        MANUAL_OVERRIDE=EEPROM.read(commom_info_addr+2);
@@ -161,9 +145,10 @@ void status_update(unsigned short int switch_number, boolean switch_state)
 		 digitalWrite(S[switch_number].pin, (switch_state==true)?HIGH:LOW);	//send control signal to switch ON/OFF a switch
 		 S[switch_number].state=switch_state;					//update state in switch's element in structure
 		 EEPROM.write(S[switch_number].addr, S[switch_number].state);		//update state in EEPROM
-		 /*Illustration to write another info regarding S1, say ID into EEPROM:
-		 EEPROM.write(S[switch_number].addr+1, S[switch_number].id); 
-		 assuming id is an element of switches structure and is defined/passed*/
+		 
+		/*Illustration to write another info regarding S1, say ID into EEPROM:
+		EEPROM.write(S[switch_number].addr+1, S[switch_number].id); 
+		assuming id is an element of switches structure and is defined/passed*/
 		
 }
 
@@ -171,23 +156,6 @@ void receive_data()
 {
     unsigned char rec_val[32];
     unsigned char len=sizeof(rec_val);
-/*    bool done = false;
-    while (!done)
-    {
-      done = radio.read( rec_val, 32*sizeof(unsigned char) );
-      Serial.print("Received data on Pipe No:");
-      Serial.println(pipeno[0]);
-      Serial.println("Received data:");
-      for(unsigned short int index=0;index<32;index++)
-      {
-       Serial.print(rec_val[index]);
-       rec_val_new[index]=rec_val[index];
-      }
-      Serial.println("");
-      data_to_be_processed=true;
-      delay(20);
-    }
-*/
     if (rf22.recv(rec_val, &len))
     {
       Serial.print("Received data: ");
@@ -214,7 +182,7 @@ void process_received_data()
        for(i=0,j=0,k=0;i<32;i++)      
        {
        if(rec_val_new[i]==',')
-         { //Serial.print("Found ',' at rec_val_new[");Serial.print(i);Serial.println("]");Serial.print("Attribute ");Serial.print(k);Serial.print(" : ");Serial.print(j);Serial.print(" to ");
+         { // Serial.println("DEBUG:");: Serial.print("Found ',' at rec_val_new[");Serial.print(i);Serial.println("]");Serial.print("Attribute ");Serial.print(k);Serial.print(" : ");Serial.print(j);Serial.print(" to ");
             l=0;
             while(j<i)
                {
@@ -223,7 +191,7 @@ void process_received_data()
                   l++;
                   j++;    
                }
-      //         Serial.println(j);
+      //     Serial.println("DEBUG:");:    Serial.println(j);
             j++;  //to skip the ','
             k++;  //to store next attribute in next row of attribute[][]
          }
@@ -234,15 +202,15 @@ void process_received_data()
    for(k=0;k<4;k++)
    {//Serial.println("");
      for(l=0;l<10;l++)
-     {//Serial.print("k=");Serial.print(k);Serial.print(",l=");Serial.println(l);
+     {// Serial.println("DEBUG:");: Serial.print("k=");Serial.print(k);Serial.print(",l=");Serial.println(l);
        if(attribute[k][l]!=0)
          {
            if(attribute[k][l]=='=')
-           {//Serial.print("Found '=' at ");Serial.print("k=");Serial.print(k);Serial.print(" l=");Serial.println(l);
+           {// Serial.println("DEBUG:");: Serial.print("Found '=' at ");Serial.print("k=");Serial.print(k);Serial.print(" l=");Serial.println(l);
              m=0;
              i=0;
              while(m<l)
-             {//Serial.print("Copying attribute[");Serial.print(k);Serial.print("][");Serial.print(m);Serial.print("] to attr_name[");Serial.print(k);Serial.print("][");Serial.print(i);Serial.println("]");
+             {// Serial.println("DEBUG:");: Serial.print("Copying attribute[");Serial.print(k);Serial.print("][");Serial.print(m);Serial.print("] to attr_name[");Serial.print(k);Serial.print("][");Serial.print(i);Serial.println("]");
                attr_name[k][i]=attribute[k][m];
                i++;
                m++;
@@ -250,7 +218,7 @@ void process_received_data()
              m++;
              j=0;
              while(attribute[k][m]!=0)
-             {//Serial.print("Copying attribute[");Serial.print(k);Serial.print("][");Serial.print(m);Serial.print("] to attr_val[");Serial.print(k);Serial.print("][");Serial.print(j);Serial.println("]");
+             {// Serial.println("DEBUG:");: Serial.print("Copying attribute[");Serial.print(k);Serial.print("][");Serial.print(m);Serial.print("] to attr_val[");Serial.print(k);Serial.print("][");Serial.print(j);Serial.println("]");
                attr_val[k][j]=attribute[k][m];
                j++;
                m++;
@@ -270,7 +238,7 @@ void process_received_data()
      for(l=0;l<6;l++)
      {
        if(attr_name[k][l]!=0)
-       {//Serial.print("attr_name[");Serial.print(k);Serial.print("][");Serial.print(l);Serial.print("]= "); 
+       {// Serial.println("DEBUG:"); Serial.print("attr_name[");Serial.print(k);Serial.print("][");Serial.print(l);Serial.print("]= "); 
          Serial.print(attr_name[k][l]);
          Serial.print(" ");
          received_attributes_list[k]=received_attributes_list[k]+attr_name[k][l];
@@ -287,63 +255,19 @@ void process_received_data()
      for(l=0;l<4;l++)
      {
        if(attr_val[k][l]!=0)
-       {//Serial.print("attr_val[");Serial.print(k);Serial.print("][");Serial.print(m);Serial.print("]= ");
+       {//Serial.println("DEBUG:");Serial.print("attr_val[");Serial.print(k);Serial.print("][");Serial.print(m);Serial.print("]= ");
          Serial.print(attr_val[k][l]);
          Serial.print(" ");
- //        received_values_list[k]=String(received_values_list[k]+attr_val[k][l]);
+ //      Serial.println("DEBUG:");  received_values_list[k]=String(received_values_list[k]+attr_val[k][l]);
        }
      }
      Serial.println();
-  //   Serial.print("received_values_list[");Serial.print(k);Serial.print("] = ");Serial.println(received_values_list[k]);
+  // Serial.println("DEBUG:");  Serial.print("received_values_list[");Serial.print(k);Serial.print("] = ");Serial.println(received_values_list[k]);
    }
    
    
-  //Infer attribute name and store in a format suitable for comparison
-/*  for(i=0,num_properties=0,num_commands=0;received_attributes_list[i]!=0;i++)
-  {
-    if(received_attributes_list[i] == "NDID" || received_attributes_list[i] == "MSID" || received_attributes_list[i] == "KPAL" || received_attributes_list[i] == "CMID")
-    {
-      num_properties++;
-      received_properties[i]=received_attributes_list[i];
-    }
-    else if(received_attributes_list[i] == "STST" || received_attributes_list[i] == "SDST" || received_attributes_list[i] == "SWMN" || received_attributes_list[i] == "SWRM")
-    {
-      num_commands++;
-      received_commands[i]=received_attributes_list[i];
-    }
-  }
-*/
-   
  for(i=0,j=0,k=0;received_attributes_list[i]!=0;i++)
  {
-/* switch(received_attributes_list[i])
- {
-   case "MSID": MASTER_ID_received=attr_val[i][0];
-                num_properties++;
-                break;
-   case "NDID": NODE_ID_received=attr_val[i][0];
-                num_properties++;
-                break;
-   case "KPAL"://code to be written
-                num_properties++;
-                break;
-   case "CMID"://code to be written
-                num_properties++;
-                break;
-   case "STST": SET_STATE_received=attr_val[i][0];
-                num_commands++;
-                break;
-   case "SDST"://code to be written
-                num_commands++;
-                break;
-   case "SWMN"://code to be written
-                num_commands++;
-                break;
-   case "SWRM"://code to be written
-                num_commands++;
-                break;   
- }
-*/
   if(received_attributes_list[i] == "MSID")
   {
     received_properties[j]=received_attributes_list[i];
@@ -413,7 +337,7 @@ unsigned char get_state()
 {
   //code to infer state from current sensors to be written
   //temporarily hard-coded for testing
-  LIVE_STATE=0x00;      // Binary: 00001000 - Switche S3 is ON, S0, S1 and S2 are off.
+  LIVE_STATE=0x00;      // Binary: 00001000 - Switch S3 is ON, S0, S1 and S2 are off.
   return LIVE_STATE;
   
 }
@@ -424,6 +348,7 @@ void send_to_server(unsigned short int send_code)
       0  -  send state to server
       1  -  
   */   
+  //TODO: Should call send_data to send?
   switch(send_code)
   {
     case 0:LIVE_STATE=get_state();
@@ -438,26 +363,17 @@ void send_to_server(unsigned short int send_code)
 }
 
 void send_data(char* data_to_send)
-{///       radio.stopListening();
-///        radio.openWritingPipe(pipes[0]);
-///        bool ok = radio.write( data_to_send, 32*sizeof(char) );  
-///        if(ok)
-      rf22.send((unsigned char*)data_to_send, sizeof(data_to_send));
+{     rf22.send((unsigned char*)data_to_send, sizeof(data_to_send));
       rf22.waitPacketSent();
-        Serial.println("Sending ok");
-// /       else
-///        Serial.println("sending failed");
-        //code to send data
- 
-///        radio.openReadingPipe(1,pipes[1]);
-///        radio.startListening();
-  
+      Serial.println("Sending ok");
+	  //TODO: Verify sending. Not tested
+	  //TODO: Add condition to check success of sending
 }
 
 void clear_all()
 {
   
-  //Function to clear transaction specific data like NODE_ID_received, MASTER_ID_received etc
+  //TODO: Function to clear transaction specific data like NODE_ID_received, MASTER_ID_received etc
   
   
   
@@ -465,7 +381,7 @@ void clear_all()
 
 void resetNode()
 {
- //Function to cater to external reset switch 
+ //TODO: Function to cater to external reset switch 
 }
 
 
@@ -476,7 +392,7 @@ void setup() //Initialization
 {
 //  attributes=attributes+String("llo");
         attachInterrupt(0, resetNode, RISING);
-	Node_Init();	//Initialize node to previous state. 	
+		Node_Init();	//Initialize node to previous state. 	
         RF_Init();
 }
 
@@ -534,7 +450,7 @@ void loop()
               else if(received_commands[i] == "SDST")
               {
                   send_to_server(0);
-                  //code to be written
+                  //TODO: code to be written to send live state to server
               }
               else if(received_commands[i] == "SWMN")
               {
@@ -546,11 +462,11 @@ void loop()
               }
             }
           }
-            else
-            {//MASTER_ID matches, but NODE_ID does not match=>retransmit
+          else
+          {//MASTER_ID matches, but NODE_ID does not match=>retransmit
              Serial.println("NODE ID does not match! Re-transmitting");
               send_data(data_to_send);
-            }
+          }
         }
           else
           {
